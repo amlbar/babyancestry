54a84ae5a0f5efaf4bfdc0d01d451ba6a998e7dc
amlbar <amielbarino@gmail.com>
2015-05-12 07:31:49 +0800

    page load improvement

--- babyancestry/apps/family/views.py
+++ babyancestry/apps/family/views.py
@@ -29,29 +29,30 @@ def family_tree(request):
 
 def get_ancestor(request, person_id):
     fs = fsClient(request)
+    # print(person_id)
 
     gender = request.GET.get('gender', '')
 
-    ancestors = fs.get(fs.ancestry(person_id))['response']
+    ancestors = fs.get(fs.ancestry(person_id, generations = 5))['response']
 
     persons = []
     for i, val in enumerate(ancestors['persons']):
         person = ancestors['persons'][i]
         person_gender = (person['display']['gender']).lower()
 
+        # ancestors are composed of both boys and girls.
+        # if we want to return only the boys then do query on person_memories only when a person's gender = male
+        # for girls, do query on person_memories only when a person's gender = female
+        # for both boys and girls, do query on person_memories for each person in ancestors
         if gender == person_gender or gender in ['all', '']:
-            person_memories = fs.get(fs.person_memories(person['id']))['response']
-            if person_memories:
-                person['stories'] = []
-                has_image = False
-                for src_desc in person_memories['sourceDescriptions']:
-                    if src_desc['mediaType'] == 'text/plain':
-                        person['stories'].append(src_desc)
-                    elif src_desc['mediaType'] in ['image/jpeg','image/png'] and not has_image:
-                        person['thumbnail'] = src_desc['links']['image-thumbnail']['href']
-                        has_image = True
-
             persons.append(person)
+
+    # just for reviewing your json output. will be removed in the future
+    format = request.GET.get('format', '').lower()
+    if format and format == 'json':
+        result = json.dumps(persons,indent=4)
+        return HttpResponse(result,content_type="json")
+
 
     return render(request, 'family/ancestry.html', {
         'ancestry': persons
@@ -79,8 +80,7 @@ def get_person_data(request, person_id):
     format = request.GET.get('format', '').lower()
     if format and format == 'json':
         result = json.dumps(person,indent=4)
-        result2 = json.dumps(memories,indent=4)
-        return HttpResponse(result2,content_type="json")
+        return HttpResponse(result,content_type="json")
 
     return render(request, 'family/person_info.html', {
         'person': person,
